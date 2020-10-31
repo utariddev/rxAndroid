54395ffe2f2de8c6a0d34c13dd8f862788fe88c5
utarid <utarid@protonmail.com>
2020-10-31 23:43:42 +0300

    view binding eklendi

--- app/src/main/java/org/utarid/rxandroid/MainActivity.java
+++ app/src/main/java/org/utarid/rxandroid/MainActivity.java
@@ -3,12 +3,17 @@ package org.utarid.rxandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import org.utarid.rxandroid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
