9cbde9068ab37d917f96b62f9cdc13e4b8449b83
utarid <utarid@protonmail.com>
2020-11-05 07:41:40 +0300

    create eklendi

--- app/src/main/java/org/utarid/rxandroid/ObservableManager.java
+++ app/src/main/java/org/utarid/rxandroid/ObservableManager.java
@@ -4,10 +4,25 @@ import io.reactivex.Observable;
 
 public class ObservableManager {
 
+    //private final List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+    String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
     private Observable<String> numbersObservable;
 
     public ObservableManager() {
-        numbersObservable = Observable.just("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+//        numbersObservable = Observable.just("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+//        numbersObservable = Observable.fromArray(numbers);
+
+        numbersObservable = Observable.create(emitter -> {
+            for (String number : numbers) {
+                if (!emitter.isDisposed()) {
+                    emitter.onNext(number);
+                }
+            }
+
+            if (!emitter.isDisposed()) {
+                emitter.onComplete();
+            }
+        });
     }
 
     public Observable<String> getNumbersObservable() {
